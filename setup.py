1ecc46713004f21d4cf6340946d38f3708bbf546
ClearlyDazed <32040230+ClearlyDazed@users.noreply.github.com>
2020-10-14 15:04:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ import setuptools
 import samsungctl
 
 setuptools.setup(
-    name=samsungctl-plus.__title__,
-    version=samsungctl-plus.__version__,
-    description=samsungctl-plus.__doc__,
-    url=samsungctl-plus.__url__,
-    author=samsungctl-plus.__author__,
-    author_email=samsungctl-plus.__author_email__,
-    license=samsungctl-plus.__license__,
+    name=samsungctl_plus.__title__,
+    version=samsungctl_plus.__version__,
+    description=samsungctl_plus.__doc__,
+    url=samsungctl_plus.__url__,
+    author=samsungctl_plus.__author__,
+    author_email=samsungctl_plus.__author_email__,
+    license=samsungctl_plus.__license__,
     long_description=open("README.rst").read(),
     entry_points={
-        "console_scripts": ["samsungctl-plus=samsungctl.__main__:main"]
+        "console_scripts": ["samsungctl_plus=samsungctl.__main__:main"]
     },
-    packages=["samsungctl-plus"],
+    packages=["samsungctl_plus"],
     install_requires=[],
     extras_require={
         "websocket": ["websocket-client"],

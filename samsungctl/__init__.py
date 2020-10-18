eecf0963340b0451c7bfe5efbaa4239452bda92e
ClearlyDazed <32040230+ClearlyDazed@users.noreply.github.com>
2020-10-18 09:51:12 -0500

    Update __init__.py

--- samsungctl/__init__.py
+++ samsungctl/__init__.py
@@ -2,9 +2,9 @@
 
 from .remote import Remote
 
-__title__ = "samsungctlPLUS"
-__version__ = "0.1.1"
-__url__ = "https://github.com/ClearlyDazed/samsungctlPLUS"
+__title__ = "samsungctl-F"
+__version__ = "0.1.0"
+__url__ = "https://github.com/ClearlyDazed/samsungctl-F"
 __author__ = "Lauri Niskanen"
 __author_email__ = "ape@ape3000.com"
 __license__ = "MIT"

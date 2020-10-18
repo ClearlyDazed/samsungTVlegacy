7500813173ab3f3644db271599e18ea51fd6a629
ClearlyDazed <32040230+ClearlyDazed@users.noreply.github.com>
2020-10-18 15:19:54 -0500

    Update __init__.py

--- samsungctl_legacy/__init__.py
+++ samsungctl_legacy/__init__.py
@@ -2,9 +2,9 @@
 
 from .remote import Remote
 
-__title__ = "samsungctl-legacy"
-__version__ = "0.1.2"
-__url__ = "https://github.com/ClearlyDazed/samsungctl-legacy"
+__title__ = "samsunglegacy"
+__version__ = "0.1.3"
+__url__ = "https://github.com/ClearlyDazed/samsunglegacy"
 __author__ = "originally Lauri Niskanen"
 __author_email__ = "ape@ape3000.com"
 __license__ = "MIT"

5642bafee83f8be25a047fb4b9550799da0fc868
ClearlyDazed <32040230+ClearlyDazed@users.noreply.github.com>
2020-10-19 15:11:31 -0500

    Update __init__.py

--- samsungTVlegacy/__init__.py
+++ samsungTVlegacy/__init__.py
@@ -3,7 +3,7 @@
 from .remote import Remote
 
 __title__ = "samsungTVlegacy"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __url__ = "https://github.com/ClearlyDazed/samsungTVlegacy"
 __author__ = "originally Lauri Niskanen"
 __author_email__ = "ape@ape3000.com"

2c2bf5e8332e794f5e08db2d14a0d9d8f67bcfb7
ClearlyDazed <32040230+ClearlyDazed@users.noreply.github.com>
2025-02-07 15:48:28 -0600

    Add files via upload

---
+++ samsungTVlegacy/__init__.py
@@ -0,0 +1,10 @@
+"""Remote control Samsung televisions via TCP/IP connection"""
+
+from .remote import Remote
+
+__title__ = "samsungTVlegacy"
+__version__ = "0.1.1"
+__url__ = "https://github.com/ClearlyDazed/samsungTVlegacy"
+__author__ = "originally Lauri Niskanen"
+__author_email__ = "ape@ape3000.com"
+__license__ = "MIT"
